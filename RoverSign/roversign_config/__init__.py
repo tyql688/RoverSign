767c56801fbb726cb140766fd5d93f6b8a45a402
tyql688 <yury.zero.21@gmail.com>
2025-08-13 11:00:29 +0800

    🐛修正自动签到

--- RoverSign/roversign_config/__init__.py
+++ RoverSign/roversign_config/__init__.py
@@ -4,9 +4,7 @@ from gsuid_core.models import Event
 from gsuid_core.sv import SV, get_plugin_available_prefix
 
 from ..utils.database.models import WavesBind
-from .set_config import (
-    set_config_func,
-)
+from .set_config import set_config_func
 
 sv_rover_config = SV("RoverSign配置")
 
@@ -16,6 +14,9 @@ PREFIX = get_plugin_available_prefix("RoverSign")
 
 @sv_rover_config.on_prefix(("开启", "关闭"))
 async def open_switch_func(bot: Bot, ev: Event):
+    if ev.text != "自动签到":
+        return
+
     at_sender = True if ev.group_id else False
     uid = await WavesBind.get_uid_by_game(ev.user_id, ev.bot_id)
     if uid is None:
@@ -26,13 +27,13 @@ async def open_switch_func(bot: Bot, ev: Event):
 
     from ..utils.rover_api import rover_api
 
-    token, token_status = await rover_api.get_self_token(uid, ev.user_id, ev.bot_id)
+    token = await rover_api.get_self_waves_ck(uid, ev.user_id, ev.bot_id)
     if not token:
         from ..utils.errors import WAVES_CODE_101_MSG
 
         return await bot.send(f"当前特征码：{uid}\n{WAVES_CODE_101_MSG}")
 
-    logger.info(f"[{ev.user_id}]尝试[{ev.command[2:]}]了[{ev.text}]功能")
+    logger.info(f"[{ev.user_id}]尝试[{ev.command[0:2]}]了[{ev.text}]功能")
 
     im = await set_config_func(ev, uid)
     await bot.send(im, at_sender)
